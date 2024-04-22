ed8da6da1b38256de556e4dc0efe029019ff3298
Michael Rosenbaum <michaelr526@gmail.com>
2024-04-22 10:09:20 -0500

    Initial cleaning file templates

---
+++ model/clean.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+from typing import List, Union
+
+
+def load_data(file_name) -> pd.DataFrame:
+    '''
+    This command loads .csv files from the Kaggle database and creates a 
+    pandas dataframe with labeled columns.
+
+    Input:
+        file_path (str): File path to data file loaded by the 
+
+    Returns (DataFrame): A pandas dataframe with consistent labels.
+    '''
+    pass
